e54db5a7f6fbeb1bbf5aa082a14828c0c95cb3b5
kathir6055 <145640357+kathir6055@users.noreply.github.com>
2024-06-08 12:54:58 +0530

    Add files via upload

---
+++ autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi I am 'kathir' Welcome to my portfolio website! I'm a passionate and aspiring software engineer with a strong desire to create innovative solutions and push the boundaries of technology.With a deep love for coding and problem-solving, I am constantly seeking opportunities to learn and grow in this ever-evolving field <br><br> Sathyamangalam"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
